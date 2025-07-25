7529e8443b9d45166a7842bd4f787127310763e3
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-07-26 01:33:14 +0500

    Update db.js

--- backened/config/db.js
+++ backened/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
+    console.log(" MongoDB connected");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
